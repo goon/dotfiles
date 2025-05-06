9fe983a1543efef935cd4fe4849079a1d7f02813
goon <hayhurst@protonmail.com>
2025-05-06 19:54:43 +0000

    ✨ initial commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,5 +29,14 @@ alias search='pacman -Ss'
 alias c='clear'
 
 alias nv='nvim'
-alias y='yazi'
 alias f='fzf'
+
+# YAZI SHELL WRAPPER 
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        width = 50,
+        sections = function()
+          local header = [[
+      ████ ██████           █████      ██                    
+     ███████████             █████                            
+     █████████ ███████████████████ ███   ███████████  
+    █████████  ███    █████████████ █████ ██████████████  
+   █████████ ██████████ █████████ █████ █████ ████ █████  
+ ███████████ ███    ███ █████████ █████ █████ ████ █████ 
+██████  █████████████████████ ████ █████ █████ ████ ██████
+]]
+
+          -- stylua: ignore
+          return {
+            { padding = 2, align = "center", text = { header, hl = "header" } },
+            { padding = 1, key = "f", desc = "Find File",       action = ":lua Snacks.dashboard.pick('files')" },
+            { padding = 1, key = "n", desc = "New File",        action = ":ene | startinsert" },
+            { padding = 1, key = "e", desc = "Recent Files",    action = ":lua Snacks.dashboard.pick('files')" },
+            { padding = 1, key = "r", desc = "Recent Projects", action = ":lua Snacks.dashboard.pick('projects')" },
+            { padding = 1, key = "s", desc = "Restore Session", section = "session" },
+            { padding = 1, key = "c", desc = "Config",          action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { padding = 1, key = "l", desc = "Lazy",            action = ":Lazy" },
+            { padding = 1, key = "x", desc = "Lazy Extras",     action = ":LazyExtras" },
+            { padding = 1, key = "q", desc = "Quit",            action = ":qa" },
+            { section = "startup" },
+          }
+        end,
+      },
+    },
+  },
+}
